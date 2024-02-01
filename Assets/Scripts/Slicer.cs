69cf42ec5dca2e70ac2a08fec3db2856c11a161b
BigCoderDominykas <dominykasgri@gmail.com>
2024-02-01 17:07:57 +0200

    Slicer

---
+++ Assets/Scripts/Slicer.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Slicer : MonoBehaviour
+{
+    Rigidbody2D rb;
+    
+    void Start()
+    {
+        if(!Application.isEditor)
+            Cursor.visible = false;
+
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        var worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        worldPos.z = 0;
+        rb.MovePosition(worldPos);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(collision.gameObject);
+    }
+}
