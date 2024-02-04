84649ff7327abad68c329124e30c34d9b7405226
BigCoderDominykas <dominykasgri@gmail.com>
2024-02-04 15:33:10 +0200

    Fruit explode particles added

--- Assets/Scripts/Slicer.cs
+++ Assets/Scripts/Slicer.cs
@@ -21,6 +21,7 @@ public class Slicer : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Destroy(collision.gameObject);
+        var fruit = collision.gameObject.GetComponent<Fruit>();
+        fruit.Slice();
     }
 }
