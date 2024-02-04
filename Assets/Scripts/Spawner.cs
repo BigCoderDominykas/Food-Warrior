f60c2f15771e2b280ab1d64cba2523723b71635c
BigCoderDominykas <dominykasgri@gmail.com>
2024-02-04 14:52:54 +0200

    Bombs added

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -3,17 +3,24 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     public GameObject fruit;
-    public float spawnDelay = 1f;
+    public GameObject bomb;
+    public float bombChance = 20;
+    public float spawnDelay = 2f;
+    float delay;
 
     void Start()
     {
-        InvokeRepeating("SpawnFruit", spawnDelay, spawnDelay);
+        delay = spawnDelay;
+        InvokeRepeating("SpawnFruit", spawnDelay, delay);
     }
 
     void SpawnFruit()
     {
+        var prefab = Random.Range(0, 100) < 100 - bombChance ? fruit : bomb;
+
+        delay = spawnDelay + Random.Range(-1f, 1f);
         var pos = transform.position;
         pos.x += Random.Range(-6f, 6f);
-        Instantiate(fruit, pos, Quaternion.identity);
+        Instantiate(prefab, pos, Quaternion.identity);
     }
 }
